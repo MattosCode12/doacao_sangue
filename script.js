d864c82b4d4376a974b05afbe19f0cd537fb67cd
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-04-06 08:41:36 -0300

    ADD - let

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ const mensagem = document.getElementById("mensagem");
 const lista = document.getElementById("listaDoadores");
 
 form.addEventListener("submit", function(e) {
-e.preventDefault();
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipo = document.getElementById("tipoSanguineo").value;
+    let telefone = document.getElementById("telefone").value.trim();
+    let cidade = document.getElementById("cidade").value.trim();
+    let estado = document.getElementById("estado").value.trim();
+
+    
